146f638389ba475d71fa1d3bf1065ba52cb4a171
xxxhdpi <xxxhdpi@naver.com>
2020-06-21 17:42:14 +0900

    first commit

---
+++ script/script.js
@@ -0,0 +1,11 @@
+$('.my-services ul li .card').on('mouseenter', function () {
+    $(this).addClass('hover-effect')
+}).on('mouseleave', function () {
+    $(this).removeClass('hover-effect')
+})
+
+$('.my-expertises .card').on('mouseenter', function () {
+    $(this).addClass('up')
+}).on('mouseleave', function () {
+    $(this).removeClass('up')
+})
